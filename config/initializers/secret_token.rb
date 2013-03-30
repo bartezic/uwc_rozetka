84ba68856f9fc79ebff9376a6018716ce41d9ef1
Vladimir Myskov <jaromudr@gmail.com>
2013-03-30 16:29:00 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rozetka::Application.config.secret_token = '7169523ba0871bce7856639963928b9fc71391408825ade58967a1bf15457886d4eddd2118a915481b30d885469dc026b432be9f8f898df411cf045fd840484d'
